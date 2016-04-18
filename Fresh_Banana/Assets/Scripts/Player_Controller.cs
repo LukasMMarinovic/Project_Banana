80045965cfbae37290c09cd21fde94d98c8b94b6
LukasMMarinovic <lukas@agramer.com>
2016-04-17 20:17:59 -0500

    alpha_v0.0.1
    +test level
    +imaginary character
    +jump
    +move left and right
    +physics

---
+++ Fresh_Banana/Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight);
+        }
+
+        //move right
+        {
+        }
+
+        //move left
+        {
+        }
+    }
+}
