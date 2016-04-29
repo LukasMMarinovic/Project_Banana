c94bd7d086c1374203ee696b57951fa6b72121d4
LukasMMarinovic <lukas@agramer.com>
2016-04-28 19:37:34 -0500

    alpha_v0.0.7
    +monkeys (they will steal your bananas and kill you)
    +crates that can be pushed
    +a bit more terrain once again

---
+++ Fresh_Banana/Assets/Scripts/MonkeyControl.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonkeyControl : MonoBehaviour
+{
+    public float moveSpeed;
+    public bool movingRight;
+
+    public float checkRadius;
+    public LayerMask consideredAsWall;
+    private bool onWall;
+    public Transform wallChecker;
+    public Vector2 wallCheck;
+
+    private bool notAtEdge;
+    public Transform edgeChecker;
+    public Vector2 edgeCheck;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        edgeCheck = edgeChecker.position;
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck, checkRadius, consideredAsWall);
+
+
+
+        wallCheck = wallChecker.position;
+        onWall = Physics2D.OverlapCircle(wallCheck, checkRadius, consideredAsWall);
+
+        if(onWall || !notAtEdge)
+        {
+            movingRight = !movingRight;
+        }
+
+
+
+
+        if (movingRight)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
